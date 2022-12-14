fe72d5d11166ac2ec3a73e253d97be2da41cbac1
Andrew Neely <andrewneely36@gmail.com>
2022-12-14 15:32:49 +0000

    finalized code

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
-import Jquery from 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js';
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+function Cr() {
+    return (
+        <div>
+            <p>P.S.</p>
+            <p>Lukas is also bad at playing Zenyatta. I carried him hard.</p>
+            <p>© Andrew™</p>
+            <br /> <br />
+        </div>);
+}
 
 function Movingradio(props) {
     return (
         <div className="radioclicky">
-            <input type="radio" id={props.id} value={props.value}></input>
+            <input type="radio" id={props.id} value={props.value} name="rating"></input>
             <label htmlFor="noradio">{props.text}</label>
         </div>
     );
@@ -16,13 +24,13 @@ function Movingradio(props) {
 function Radio(props) {
     return (
         <div className="radioclicky">
-            <input type="radio" id={props.id} value={props.value}></input>
+            <input type="radio" id={props.id} value={props.value} name="rating"></input>
             <label htmlFor="noradio">{props.text}</label>
         </div>
     );
 }
 
-function Annoyingform(props) {
+function Annoyingform() {
     return (<div className="formpos"><div className="rainbow-border" id="annoyingarea"><form>
         <br />
         <label htmlFor="firstname" className="textlabel">First Name</label>
@@ -32,12 +40,27 @@ function Annoyingform(props) {
         <input type="text" id="lastname" />
         <br /> <br />
         <p>IS POKEMON GOOD</p>
-        <Movingradio id="yesradio" text="Yes" value="yes" onClick={()=>move(this)}/>
-        <Radio id="noradio" text="No" value="no"/>
+        <Movingradio id="yesradio" text="Yes" value="yes" />
+        <Radio id="noradio" text="No" value="no" />
         <input type="submit" value="Submit" /></form></div></div>)
 }
 
-function Spinningimage(props) {
+function Testbutton(){
+    const [states] = useState({initial: "Press me!", other: "Press again to reset!"});
+    const [buttontext, setText] = useState(states.initial);
+    const handleClick = () => {
+       if(buttontext === states.initial){
+        setText(states.other);
+       } else{
+        setText(states.initial);
+       }
+    }
+    return(
+    <button onClick={handleClick}>{buttontext}</button>
+    );
+}
+
+function Spinningimage() {
     return (<img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/583.png" className="rotate-image"></img>);
 }
 
@@ -49,7 +72,7 @@ function Rainbowbar() {
     return (<hr className="rainbow-hr"></hr>)
 }
 
-function Headimage(props) {
+function Headimage() {
     return (<div className="headimage"></div>)
 }
 
@@ -84,39 +107,13 @@ function Page() {
             <Rainbowbar />
             <Rainbowtext text="LUKAS HAS A BAD OPINION. THAT'S THE WHOLE CONTENT OF THE PAGE. POKEMON HASN'T BEEN GOOD IN YEARS. LIKE HONESTLY WHAT IS THIS THING!?!?!?!?!?!?!?!?" />
             <Spinningimage />
+            <Testbutton />
             <Annoyingform />
+            <br /> <br />
+            <Cr />
         </div>
     );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Page />)
-
-
-
-// function generate(size) {
-//     var random = ((Math.random() * size) / 2.5);
-//     console.log(random);
-//     random *= Math.round(Math.random()) ? 1 : -1;
-//     return random;
-// }
-
-// function move(a) {
-//     var width = document.getElementById("annoyingarea").width();
-//     var height = document.getElementById("annoyingarea").height();
-
-//     width = width - (width * 0.04);
-//     height = height - (height * 0.1);
-
-//     var newPos = new function () {
-//         this.left;
-//         this.top;
-//     };
-
-//     newPos.left = generate(width);
-//     newPos.top = generate(height);
-//     a.animate({
-//         "top": newPos.top + "px",
-//         "left": newPos.left + "px"
-//     }, 500);
-// }
